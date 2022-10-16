e9d3344a036399f0b4580e0cf23e624f5cff1199
kjuiop <copying_y@naver.com>
2022-10-16 15:27:44 +0900

    feat: springBoot Application 실행 클래스 생성

--- spring-rest-docs-mockmvc/src/main/java/com/gig/restdocs/mockmvc/Application.java
+++ spring-rest-docs-mockmvc/src/main/java/com/gig/restdocs/mockmvc/Application.java
@@ -1,8 +1,17 @@
 package com.gig.restdocs.mockmvc;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author : JAKE
  * @date : 2022/10/16
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
